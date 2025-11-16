203a29cc89aa0b8934623b0716e992033be87803
codecanna <codecannamw@gmail.com>
2025-11-16 00:00:00 -0700

    Added tests for all functions.

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,47 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { E621Bot } from "./models/E621Bot.ts";
+import { E621RequestBuilder } from "./models/E621RequestBuilder.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function buildUrlTest() {
+  const testUrl =
+    "https://e621.net/posts.json?tags=dragon+rating:safe&limit=10";
+  const testUrlBuilder = new E621RequestBuilder("10");
+  testUrlBuilder.tags = ["dragon"];
+  testUrlBuilder.rating = "rating:safe";
+  assertEquals(testUrlBuilder.buildUrl(), testUrl);
+});
+
+Deno.test(function tagStringTest() {
+  const testTagString = "dragon+unicorn+rating:safe";
+  const testUrlBuilder = new E621RequestBuilder("10");
+  testUrlBuilder.tags = ["dragon", "unicorn", "rating:safe"];
+  assertEquals(testUrlBuilder.tagString(), testTagString);
+});
+
+Deno.test(function getFileExtensionsTest() {
+  const testFile = "test-file.txt";
+  const testUrlBuilder = new E621RequestBuilder();
+  assertEquals(testUrlBuilder.getFileExtensions(testFile), "txt");
+});
+
+Deno.test(async function parseInlineQueryTest() {
+  const testUrlBuilder = new E621RequestBuilder();
+  testUrlBuilder.tags = ["dragon", "unicorn"];
+  testUrlBuilder.rating = "rating:safe";
+  const testBot = new E621Bot("TEST_TOKEN", "TEST_TOKEN");
+  assertEquals(
+    await testBot.parseInlineQuery(
+      "dragon unicorn safe",
+      new E621RequestBuilder(),
+    ),
+    testUrlBuilder,
+  );
+});
+
+Deno.test(async function sendRequestTest() {
+  const testUrl = "https://e621.net/posts?tags=dragon+rating:safe";
+  const testBot = new E621Bot("TEST_TOKEN", "TEST_TOKEN");
+  const testResponse = await testBot.sendRequest(testUrl);
+  await testResponse.body?.cancel(); // Cancel test request
+  assertEquals(testResponse.status, 401);
 });
