98f0b30449e7f7ef02a0d7393f0ac5b6e9989194
codecanna <codecannamw@gmail.com>
2025-11-18 19:11:21 -0700

    added MAX_FILE_SIZE and comments.

--- constants/numbers.ts
+++ constants/numbers.ts
@@ -1,2 +1,14 @@
+/**
+ * Max number of images Telegram can load at a time
+ */
 export const IMAGE_LOAD_COUNT: number = 50;
+
+/**
+ * Max file size in megabytes that the bot can upload (Limit set by Telegram)
+ */
+export const MAX_FILE_SIZE: number = 50;
+
+/**
+ * Maximum number of posts the bot can pull per request
+ */
 export const API_PAGE_SIZE = 320;
