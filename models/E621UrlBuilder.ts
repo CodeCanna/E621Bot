b996589809878798d176f2c02c0928edadcd06f2
codecanna <codecannamw@gmail.com>
2025-11-24 23:04:48 -0700

    Consolidated interaces to one file.

--- models/E621UrlBuilder.ts
+++
@@ -1,7 +0,0 @@
-export interface E621UrlBuilder {
-    baseUrl: string;
-    limit?: number;
-    page?: number;
-    endpoint: string;
-    buildUrl(): string;
-}
--- models/Pool.ts
+++
@@ -1,7 +0,0 @@
-export interface Pool {
-    id: number,
-    name: string,
-    url: string,
-    thumbnailUrl: string,
-    post_ids: string[]
-}
