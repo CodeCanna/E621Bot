3905623063ec06904ebb0057cd29985c3dbea012
codecanna <codecannamw@gmail.com>
2025-12-29 02:24:01 -0700

    Separated tests into multiple files.

---
+++ tests/misc_tests.ts
@@ -0,0 +1,52 @@
+import { assertEquals } from "@std/assert/equals";
+import { E621Bot } from "../models/E621Bot.ts";
+import { E621UrlBuilderPools } from "../models/E621UrlBuilderPools.ts";
+import * as urls from "../constants/urls.ts";
+
+// TODO: Write test for processPosts()
+Deno.test(function getPoolsGalleryTest() {
+  const poolsGalleryUrl =
+    `${urls.baseUrl}${urls.endpoint.json.pools}?page=1&limit=3`;
+  const testUrlBuilder = new E621UrlBuilderPools();
+
+  assertEquals(testUrlBuilder.poolsGalleryUrl(), poolsGalleryUrl);
+});
+
+Deno.test(async function sendRequestTest() {
+  const testUrl = "https://e621.net/posts?tags=dragon+rating:safe";
+  const testBot = new E621Bot(
+    Deno.env.get("TELEGRAM_BOT_KEY") || "",
+    Deno.env.get("E621_API_KEY") || "",
+    new Array<string>(),
+  );
+  const testResponse = await testBot.sendRequest(testUrl);
+  await testResponse.body?.cancel(); // Cancel test request
+  assertEquals(testResponse.status, 200);
+});
+
+Deno.test(function calcMegabytesTest() {
+  const testBot = new E621Bot(
+    Deno.env.get("TELEGRAM_BOT_KEY") || "",
+    Deno.env.get("E621_API_KEY") || "",
+    new Array<string>(),
+  );
+  const testValue = 1024; // bytes
+  assertEquals(Math.ceil(testBot.calcMegabytes(testValue)), 1);
+});
+
+Deno.test(function buildBlacklistRegexTest() {
+  const blacklist: string[] = [
+    "feces",
+    "murder",
+    "waterworks",
+  ];
+  const testBot = new E621Bot(
+    Deno.env.get("TELEGRAM_BOT_KEY") || "",
+    Deno.env.get("E621_API_KEY") || "",
+    blacklist,
+  );
+  assertEquals(
+    testBot.buildBlacklistRegex(),
+    /(feces|murder|waterworks)/,
+  );
+});
---
+++ tests/url_builder_tests.ts
@@ -0,0 +1,28 @@
+import { assertEquals } from "@std/assert/equals";
+import { API_PAGE_SIZE } from "../constants/numbers.ts";
+import { E621UrlBuilderPosts } from "../models/E621UrlBuilderPosts.ts";
+
+Deno.test(function buildUrlPostsTest() {
+  const testUrl =
+    `https://e621.net/posts.json?tags=dragon+rating:safe&page=1&limit=${API_PAGE_SIZE}`;
+  const testUrlBuilder = new E621UrlBuilderPosts();
+  testUrlBuilder.tags = ["dragon"];
+  testUrlBuilder.rating = "rating:safe";
+  assertEquals(testUrlBuilder.buildUrl(), testUrl);
+});
+
+Deno.test(function buildUrlPoolsTest() {
+});
+
+Deno.test(function tagStringTest() {
+  const testTagString = "?tags=dragon+unicorn+rating:safe";
+  const testUrlBuilder = new E621UrlBuilderPosts();
+  testUrlBuilder.tags = ["dragon", "unicorn", "rating:safe"];
+  assertEquals(testUrlBuilder.tagString(), testTagString);
+});
+
+Deno.test(function getFileExtensionsTest() {
+  const testFile = "test-file.txt";
+  const testUrlBuilder = new E621UrlBuilderPosts();
+  assertEquals(testUrlBuilder.getFileExtensions(testFile), "txt");
+});
