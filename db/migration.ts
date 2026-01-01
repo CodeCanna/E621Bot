c5091bd34a30678012031c75f4e82c34461a4a36
codecanna <codecannamw@gmail.com>
2026-01-01 00:06:06 -0700

    added ratings field

--- db/migration.ts
+++ db/migration.ts
@@ -1,14 +1,14 @@
 import { PathLike } from "node:fs";
 import { DatabaseSync } from "node:sqlite";
 
-export function createBlacklistDb(dbFile: PathLike) {
+export function createUserDb(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
-
     db.prepare(`
       CREATE TABLE IF NOT EXISTS user_db (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         telegram_id BIGINT NOT NULL UNIQUE,
+        rating TEXT DEFAULT 's', -- Default rating for the bot for now will be s (safe)
         blacklist TEXT DEFAULT 'gore,scat,watersports,young,loli,shota' -- Default blacklist for every user on e621
       );
     `).run();
